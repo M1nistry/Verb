74ba857e42b0628d79ca388863362421cbe00f5f
M1nistry <Ministry@live.com.au>
2015-04-24 14:34:12 +1000

    Initial Commit, this isn't going to go anywhere

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace GumshoeIM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var userImage = new BitmapImage
+            {
+                BaseUri = new Uri(@"C:\Users\M1nistry\Pictures\Other\me.png")
+            };
+            imageUserPicture.Source = userImage;
+        }
+
+        private void canvasHeader_ToolTipOpening(object sender, ToolTipEventArgs e)
+        {
+            e.Handled = true;
+            if (MenuMain.Height < 20) MenuMain.Height = 20;
+        }
+
+        private void Main_GotFocus(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
